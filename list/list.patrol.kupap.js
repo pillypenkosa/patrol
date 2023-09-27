f07782906e7101cb086a7e5a45c5e43a27cf492a
pillypenkosa <pillypenkosa@gmail.com>
2023-09-27 22:17:01 +0300

    Patrol 2023-09-27 21:55

---
+++ list/list.patrol.kupap.js
@@ -0,0 +1,58 @@
+
+
+
+
+
+const arrListPatrolKupap = [
+
+
+{ 
+	id 		: '122_1', 
+	title 	: '', 
+
+
+
+
+
+
+},
+
+
+
+
+
+
+
+
+
+
+
+
+
+];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+let objListAny = {};
+arrListAny.forEach( k => {
+	objListAny[ k.id ] = k;
+});
+*/
+
+
+
+
+
+
---
+++ list/list.patrol.pdr.js
@@ -0,0 +1,70 @@
+
+
+
+
+
+const arrListPatrolPdr = [
+
+
+
+
+
+
+{ 
+	id 		: '1_1_1', 
+	title 	: '', 
+},
+
+
+
+
+
+
+
+
+
+
+{ 
+	id 		: '8_7_3_e', 
+	title 	: '', 
+},
+
+
+
+
+
+
+
+
+
+
+
+
+
+];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+let objListAny = {};
+arrListAny.forEach( k => {
+	objListAny[ k.id ] = k;
+});
+*/
+
+
+
+
+
+
